a9b3dbf9f406c0cc463b202517b37305b2a41cf7
sejongpark <devsejong@gmail.com>
2016-02-16 09:13:13 +0900

    아스키독 파서 추가.

--- src/main/java/io/github/devsejong/wiki/parser/DocumentType.java
+++ src/main/java/io/github/devsejong/wiki/parser/DocumentType.java
@@ -1,14 +1,12 @@
 package io.github.devsejong.wiki.parser;
 
-import io.github.devsejong.wiki.parser.operator.HtmlParser;
-import io.github.devsejong.wiki.parser.operator.MarkdownParser;
-import io.github.devsejong.wiki.parser.operator.Parser;
-import io.github.devsejong.wiki.parser.operator.TextParser;
+import io.github.devsejong.wiki.parser.operator.*;
 
 public enum DocumentType {
     MARKDOWN(MarkdownParser.class),
     HTML(HtmlParser.class),
-    TEXT(TextParser.class);
+    TEXT(TextParser.class),
+    ASCIIDOC(AsciidocParser.class);
 
     private Class<? extends Parser> parserClass;
 
